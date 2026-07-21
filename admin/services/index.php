e7568854941bf14d8f2f8b3e9fadf82a1da82d56
Adam Annour <adamannourannouridriss@gmail.com>
2026-07-22 01:47:50 +0200

    Implement services management dashboard

---
+++ admin/services/index.php
@@ -0,0 +1,55 @@
+<?php
+include '../../database/db.php';
+
+$query = "SELECT * FROM services";
+$result = mysqli_query($conn, $query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Services</title>
+</head>
+
+<body>
+
+    <h1>Manage Services</h1>
+
+    <p>
+        <a href="create.php">+ Add New Service</a>
+    </p>
+
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Actions</th>
+        </tr>
+
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <tr>
+                <td>
+                    <?php echo $row['id']; ?>
+                </td>
+                <td>
+                    <?php echo $row['title']; ?>
+                </td>
+                <td>
+                    <?php echo $row['description']; ?>
+                </td>
+                <td>
+                    <a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a> |
+                    <a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
+
+    </table>
+
+</body>
+
+</html>
